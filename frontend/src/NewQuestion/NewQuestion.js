c5712b9ad04430e9ce00cde29fca07f94f1c1fb6
mohammed aladdin <i3laddin@gmail.com>
2020-03-16 23:19:15 +0200

    make it better

--- frontend/src/NewQuestion/NewQuestion.js
+++ frontend/src/NewQuestion/NewQuestion.js
@@ -31,7 +31,7 @@ class NewQuestion extends Component {
       disabled: true,
     });
 
-    await axios.post('http://localhost:5000/api/questions/', {
+    await axios.post('/api/questions', {
       title: this.state.title,
       description: this.state.description,
     },
