7244e0b56a9d7a932d5f1bb36514d221d8307039
mohammed aladdin <i3laddin@gmail.com>
2020-03-16 15:27:15 +0200

    make it better

---
+++ frontend/build/precache-manifest.22c8c24a923fc84a2ac86c904ba8b7a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe4072edbf423ff342095113acbff6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14bff65c4cd70b48c1e5",
+    "url": "/static/css/main.6aa3a582.chunk.css"
+  },
+  {
+    "revision": "c22df00647da3cac3dee",
+    "url": "/static/js/2.5125506a.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.5125506a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14bff65c4cd70b48c1e5",
+    "url": "/static/js/main.7f693adc.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  }
+]);
--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.post('/answer/:id', checkJwt, (req, res) => {
 });
 
 
-//if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // set static folder
 
   app.get('*', (req, res) => {
@@ -119,7 +119,7 @@ app.post('/answer/:id', checkJwt, (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", 'build', 'index.html'))
 
   })
-//}
+}
 
 const port = process.env.PORT || 5000;
 // start the server
