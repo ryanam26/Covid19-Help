90bc9146fd3462ee0cc80b1a5463443c6ae1c18e
Ryan Morrison <ryanam26@yahoo.com>
2020-03-17 16:51:14 -0400

    updated title

--- frontend/src/Questions/Questions.js
+++ frontend/src/Questions/Questions.js
@@ -38,7 +38,7 @@ class Questions extends Component {
               <div key={question.id} className="chil col-sm-12 col-md-4 col-lg-3">
                 <Link className="hover-card" to={`/question/${question.id}`}>
                   <div className="card custome-card  text-white bg-success mb-3">
-                    <div className="card-header">No. of people helping: {question.answers}</div>
+                    <div className="card-header">No. of responses: {question.answers}</div>
                     <div className="card-body">
                       <h4 className="card-title">{question.title}</h4>
                       <p className="card-text">{question.description}</p>
