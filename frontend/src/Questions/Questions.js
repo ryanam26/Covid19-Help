6ac664c6c821ebcc5271ef871874e0ca669eb347
Ryan Morrison <ryanam26@yahoo.com>
2020-03-18 15:06:49 -0400

    updated text on questions page

--- frontend/src/Questions/Questions.js
+++ frontend/src/Questions/Questions.js
@@ -23,13 +23,14 @@ class Questions extends Component {
     return (
       
       <div className="container text-center">
-        <h4 className='text-center'>Hi all, if you are affected by this pandemic as much as I am , this site is for you.
-        This site is a work in progress and I need volenteers to help and add additional features, it's built with the MERN stack.  Please reach out here: ryanam6480@gmail.com<br></br></h4>
+        <h4 className='text-center'>Hi friends!  We are all being affected by this pandemic, and this site is meant to help.
+        This is a work in progress and I need volunteers to help and add additional features - it's built with the MERN stack.  Please reach out here: ryanam6480@gmail.com<br></br></h4>
         <hr></hr>
         <h5 className='text-center'>How this site works:</h5>
-        <p className='text-center'>1. Click the grey box below and ask for help, enter the city you are located in and a simple request, e.g. I need a job or I need to pick up my elderly parents medication but I am stuck.<br></br></p>
-        <p className='text-center'>2. Add a link to your bio, email or anything the helper can use to help you.  <b>Please do not give out any sensitive information.</b></p>
-        <p className='text-center'>3. Helpers, click on the cards that you are interested in helping and submit an answer as to how you can help.</p>
+        <p className='text-center'>1. Click the grey box below and ask for help, enter the city you are located in and a simple request, e.g. I need a job, I need to pick up my elderly parents medication but I am stuck or where can I find sanitizer etc...<br></br></p>
+        <p className='text-center'>2. Add a link to your bio, email or anything the helper can use to help you.  <b>Please do not expose any sensitive information.</b></p>
+        <p className='text-center'>3. Helpers, click on the cards that you are interested in helping and submit an answer as to how you can help. <b>NO BAD ACTORS ALLOWED!</b></p>
+        <p className='text-center'>4. Please reach out with feature suggestions at the email above. <b>Good luck, be safe, and kind to one another!</b></p>
         <div className="row">
           <Link className="hover-card" to="/new-question">
             <div style={{ paddingRight: "10px" }} className="card custome-card fath text-white bg-secondary mb-3">
